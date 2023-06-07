c0a50f3d61bc57c783959d4c9d2faf661f18f611
Rémi Michel <remi.michel38@gmail.com>
2023-06-07 16:01:22 +0200

    feat(): session du 07/06

---
+++ footer.php
@@ -0,0 +1,8 @@
+</main>
+</div>
+<footer id="footer" role="contentinfo">
+</footer>
+</div>
+<?php wp_nav_menu( array( 'menu' => 'footer', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="wrapper" class="hfeed">
+<header id="header" role="banner">
+<div id="branding">
+<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+<div class="header">
+    <?php echo wp_get_attachment_image(19, 'medium') ?>
+    <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+<div>
+</nav>
+</header>
+<div id="container">
+<main id="content" role="main">
